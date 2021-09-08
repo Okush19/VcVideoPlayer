0f23749bb69713f29c60d58e43082d36f5dc3771
PRATHEEsni <89799912+PRATHEEsni@users.noreply.github.com>
2021-09-09 03:30:07 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -1,7 +1,7 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin With Full Rights` \n2) **Add** @{ASSISTANT_NAME} **To Your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply To Video Or File That You Want To Stream In Vc 😊** \n- `/stopstream`  **: Stop The Stream**"
+HELP_TEXT =f"**SETTINGS** \n\n`1) Add This Bot To Your Group And Make It Admin With Full Rights` \n2) **Add** @{ASSISTANT_NAME} **To Your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply To Video Or File That You Want To Stream In Vc 😊** \n- `/stopstream`  **: Stop The Stream**"
 START_TEXT ='Hᴇʏ Mʏ Nᴀᴍᴇ Is Eʟɪɴᴀ 💖 \n\n Lᴇᴛs Eɴᴊᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs ✨❤️ /n/n 𝐑𝐞𝐠𝐚𝐫𝐝𝐬 🥀 : @Pratheek_XD '
 ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This Bot Was Created To Stream Video In Telegram Group Video Chats Using Several Methods From Webrtc.` \n\n`💡 Powered By Pytgcalls The Async Client Api For The Telegram Group Calls, And Pyrogram The Telegram Mtproto Api Client Library And Framework In Pure Python For Users And Bots.` \n\n**This Bot Licensed Under Gnu-gpl 3.0 License**'
 DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
