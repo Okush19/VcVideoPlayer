10f991e3756e3b98e9f6601bf100728fd5265945
PRATHEEsni <89799912+PRATHEEsni@users.noreply.github.com>
2021-09-10 14:13:10 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -2,6 +2,6 @@ import os
 from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`1) Add This Bot To Your Group And Make It Admin With Full Rights` \n2) **Add** @{ASSISTANT_NAME} **To Your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply To Video Or File That You Want To Stream In Vc 😊** \n- `/stopstream`  **: Stop The Stream**"
-START_TEXT ='Hᴇʏ Mʏ Nᴀᴍᴇ Is Eʟɪɴᴀ [MY](https://telegra.ph/file/23f2c8579ec50213707a1.jpg)💖 \n\nLᴇᴛs Eɴᴊᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs ✨❤️ \n\n𝐑𝐞𝐠𝐚𝐫𝐝𝐬 🥀 : @Pratheek_XD '
+START_TEXT ='Hᴇʏ Mʏ Nᴀᴍᴇ Is Eʟɪɴᴀ (https://telegra.ph/file/23f2c8579ec50213707a1.jpg)💖 \n\nLᴇᴛs Eɴᴊᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs ✨❤️ \n\n𝐑𝐞𝐠𝐚𝐫𝐝𝐬 🥀 : @Pratheek_XD '
 ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This Bot Was Created To Stream Video In Telegram Group Video Chats Using Several Methods From Webrtc.` \n\n`💡 Powered By Pytgcalls The Async Client Api For The Telegram Group Calls, And Pyrogram The Telegram Mtproto Api Client Library And Framework In Pure Python For Users And Bots.` \n\n**This Bot Licensed Under Gnu-gpl 3.0 License**'
 DEVS_TEXT =f"**Eʟɪɴᴀ Vɪᴅᴇᴏ Bᴏᴛ** \n\n`Here Some Developers Helping in Making The` 𝐄𝐋𝐈𝐍𝐀 𝐕𝐈𝐃𝐄𝐎 𝐒𝐓𝐑𝐄𝐀𝐌𝐈𝐍𝐆 `Bot.`"
